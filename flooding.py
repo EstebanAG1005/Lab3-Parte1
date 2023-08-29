c1d65c5d4d36805934255832e07be1cfac84ef84
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2023-08-29 17:05:43 -0600

    Changes

--- flooding.py
+++ flooding.py
@@ -1,14 +1,14 @@
 class UniqueFlooding:
-
     def __init__(self, topology_file):
         self.load_topology(topology_file)
         self.me = self.pick_node()
         self.neighbors = self.topology.get(self.me, [])
         self.visited_nodes = set()
+        self.sent_messages = set()  # Nueva variable para registrar mensajes enviados
 
     def load_topology(self, topology_file):
         self.topology = {}
-        with open(topology_file, 'r') as f:
+        with open(topology_file, "r") as f:
             for line in f:
                 node, neighbors = line.strip().split(": ")
                 self.topology[node] = neighbors.split(",")
@@ -20,6 +20,7 @@ class UniqueFlooding:
     def deliver_message(self, message, target):
         print("🌟 Preparando mensaje 🌟")
         self.visited_nodes.add(self.me)
+        self.sent_messages.add(message)  # Registrar el mensaje como enviado
         self.propagate_message(message, target)
 
     def propagate_message(self, message, target):
@@ -29,30 +30,38 @@ class UniqueFlooding:
                 print(f"🚀 Enviando mensaje a: {neighbor}")
                 self.visited_nodes.add(neighbor)
 
-    def accept_message(self, message, sender, target):
+    def accept_message(self, sender, target, message=None):
+        if message in self.sent_messages:
+            print(f"🗑️ Descartando mensaje repetido: {message}")
+            return
         if self.me not in self.visited_nodes:
             self.visited_nodes.add(self.me)
             print(f"🎉 Mensaje recibido de {sender}: {message}")
             self.propagate_message(message, target)
 
+
 if __name__ == "__main__":
-    flooding = UniqueFlooding('topologia.txt')
-    options = ["Enviar Mensaje", "Recibir Mensaje", "Cerrar el Programa"]
+    flooding = UniqueFlooding("topologia.txt")
+    options = ["Enviar Mensaje", "Simular Recepción de Mensaje", "Cerrar el Programa"]
     while True:
         print("\n📜 Menú:")
         for i, opt in enumerate(options):
             print(f"{i+1}. {opt}")
-        option = input("🔮 Elige tu destino: ")
+        option = input("🔮 Elige tu Opcion: ")
 
         if option == "1":
             message = input("✒️ Escribe tu mensaje: ")
             target = input("🎯 Indica el destino: ")
             flooding.deliver_message(message, target)
         elif option == "2":
-            sender = input("💌 ¿De quién es el mensaje recibido? ")
-            message = input("📜 ¿Cuál es el mensaje? ")
+            sender = input("💌 Simular recepción: ¿De qué nodo proviene el mensaje? ")
             target = input("🎯 ¿Cuál es el destino del mensaje? ")
-            flooding.accept_message(message, sender, target)
+            message = input(
+                "💬 ¿Cuál es el mensaje? (Oprime Enter para usar el último mensaje enviado): "
+            )
+            if not message:
+                message = None
+            flooding.accept_message(sender, target, message)
         elif option == "3":
             print("👋 Cerrando el programa. ¡Hasta luego!")
             break
