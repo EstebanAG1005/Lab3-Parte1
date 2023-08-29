e07a27279cde49f10ee11f0c031c19781437a24c
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2023-08-29 11:35:33 -0600

    Changes on the code and interface

--- flooding.py
+++ flooding.py
@@ -4,6 +4,7 @@ class UniqueFlooding:
         self.load_topology(topology_file)
         self.me = self.pick_node()
         self.neighbors = self.topology.get(self.me, [])
+        self.visited_nodes = set()
 
     def load_topology(self, topology_file):
         self.topology = {}
@@ -17,42 +18,43 @@ class UniqueFlooding:
         return input("Elige tu nodo: ")
 
     def deliver_message(self, message, target):
-        print("Iniciando protocolo de envío mágico 🌟")
-        self.propagate_message(message, target, [self.me])
+        print("🌟 Preparando mensaje 🌟")
+        self.visited_nodes.add(self.me)
+        self.propagate_message(message, target)
 
-    def propagate_message(self, message, target, visited):
-        print(f"Revisando conexión mística con: {self.neighbors}")
+    def propagate_message(self, message, target):
+        print(f"🌐 Nodos conectados: {self.neighbors}")
         for neighbor in self.neighbors:
-            if neighbor not in visited:
-                print(f"Enviando runas mágicas a: {neighbor}")
-                visited.append(neighbor)
+            if neighbor not in self.visited_nodes:
+                print(f"🚀 Enviando mensaje a: {neighbor}")
+                self.visited_nodes.add(neighbor)
 
-    def accept_message(self, message, sender, visited):
-        if target == self.me:
-            print(f"¡Mensaje recibido desde {sender}! El mensaje es: {message}")
-        elif self.me in visited:
-            print("Mensaje Recibido. No reenviar.")
-        else:
-            print(f"Canalizando energías para reenviar a: {self.neighbors}")
-            visited.append(self.me)
-            self.propagate_message(message, target, visited)
-
+    def accept_message(self, message, sender, target):
+        if self.me not in self.visited_nodes:
+            self.visited_nodes.add(self.me)
+            print(f"🎉 Mensaje recibido de {sender}: {message}")
+            self.propagate_message(message, target)
 
 if __name__ == "__main__":
     flooding = UniqueFlooding('topologia.txt')
-    option = None
-    while option != "3":
-        print("\n\n📜 1. Enviar Mensaje")
-        print("📜 2. Recibir Mensaje")
-        print("📜 3. Cerrar el Programa")
+    options = ["Enviar Mensaje", "Recibir Mensaje", "Cerrar el Programa"]
+    while True:
+        print("\n📜 Menú:")
+        for i, opt in enumerate(options):
+            print(f"{i+1}. {opt}")
         option = input("🔮 Elige tu destino: ")
+
         if option == "1":
-            message = input("✒️ Inscribir mensaje: ")
-            target = input("🎯 Elige el objetivo del mensaje: ")
+            message = input("✒️ Escribe tu mensaje: ")
+            target = input("🎯 Indica el destino: ")
             flooding.deliver_message(message, target)
         elif option == "2":
-            arcane_message = input("✉️ Ingrese el mensaje de la siguiente manera, <objetivo,mensaje,emisor>: ")
-            target, message, sender = arcane_message.split(",")
-            visited_input = input("🗺 Ingresa el recorrido del mensaje de esta forma <nodo1,nodo2,nodo3> : ")
-            visited = visited_input.split(",")
-            flooding.accept_message(message, sender, visited)
+            sender = input("💌 ¿De quién es el mensaje recibido? ")
+            message = input("📜 ¿Cuál es el mensaje? ")
+            target = input("🎯 ¿Cuál es el destino del mensaje? ")
+            flooding.accept_message(message, sender, target)
+        elif option == "3":
+            print("👋 Cerrando el programa. ¡Hasta luego!")
+            break
+        else:
+            print("🚫 Opción no reconocida. Inténtalo de nuevo.")
