fa5639d3103dffa04a81a1dc0229d8d59a4d016f
EstebanAG1005 <60299338+EstebanAG1005@users.noreply.github.com>
2023-09-11 15:39:21 -0600

    changes to flooding

--- Parte2/Flooding/flooding.py
+++ Parte2/Flooding/flooding.py
@@ -34,10 +34,12 @@ class Flooding(slixmpp.ClientXMPP):
         self.flag = False
         self.run = False
         self.nodes_visited = []
+        self.sent_messages = set()
 
     def setup_logging(self, level):
-        logging.basicConfig(level=level)
-        logging.getLogger("slixmpp").setLevel(level)
+        # logging.basicConfig(level=logging.INFO)
+        # logging.getLogger("slixmpp").setLevel(level)
+        logging.basicConfig(level=logging.ERROR)
 
     async def start(self, event):
 
@@ -62,158 +64,87 @@ class Flooding(slixmpp.ClientXMPP):
             else:
                 print("Invalid option")
 
-    # When user sends flood message
-    async def flood_send(self):
-        print("Ingrese el usuario al que desea enviar el mensaje (sin @alumchat.xyz): ")
-        user = await ainput("Usuario: ")
-        user = user + "@alumchat.xyz"
-        print("Ingrese el mensaje que desea enviar")
-        message = await ainput("Mensaje: ")
-        """Nodo fuente [texto + @]
-        - Nodo destino [texto + @]
-        - Saltos (nodos) recorridos [numérico]
-        - Distancia [numérico]
-        - Listado de nodos [texto]
-        - Mensaje [texto]"""
-        msg = {}
-        msg["source"] = self.jid
-        msg["destination"] = user
-        msg["hops"] = 0
-        msg["distance"] = 0
-        msg["nodes"] = []
-        msg["message"] = message
+    async def get_input(self, prompt, suffix=""):
+        value = await ainput(prompt)
+        return value + suffix
 
+    async def flood_send(self):
+        user_domain = "@alumchat.xyz"
+        user = await self.get_input(
+            "Ingrese el usuario al que desea enviar el mensaje (sin @alumchat.xyz): ",
+            user_domain,
+        )
+        message = await self.get_input("Ingrese el mensaje que desea enviar: ")
+
+        msg = {
+            "source": self.jid,
+            "destination": user,
+            "hops": 0,
+            "distance": 0,
+            "nodes": [],
+            "message": message,
+        }
+
+        node = self.get_key_by_value(self.jid)
+        if node:
+            self.process_node(msg, node)
+        else:
+            print("No hay nodos")
+
+    def get_key_by_value(self, value):
         try:
-            print(
-                "Getting key: ",
-                list(self.json_data["config"].keys())[
-                    list(self.json_data["config"].values()).index(self.jid)
-                ],
-            )
-            node = list(self.json_data["config"].keys())[
-                list(self.json_data["config"].values()).index(self.jid)
+            return list(self.json_data["config"].keys())[
+                list(self.json_data["config"].values()).index(value)
             ]
-            receivers_node = self.topology["config"][node]
-            print("send message to nodes: ", receivers_node)
-            msg["hops"] = msg["hops"] + 1
+        except:
+            return None
+
+    def process_node(self, msg, node):
+        receivers_node = self.topology["config"].get(node)
+        if receivers_node:
+            print(f"Getting key: {node}")
+            print(f"send message to nodes: {receivers_node}")
+            msg["hops"] += 1
             msg["nodes"].append(node)
-            msg["distance"] = msg["distance"] + 1
+            msg["distance"] += 1
             msg["id"] = id(node)
+            if (
+                msg["id"] in self.sent_messages
+            ):  # Comprobamos si el mensaje ya fue enviado
+                print("Mensaje descartado porque es repetido.")
+                return
             self.flag = True
             self.nodes_visited.append(node)
             for receiver in receivers_node:
-                receiver = self.json_data["config"][receiver]
+                receiver_jid = self.json_data["config"].get(receiver)
+                if receiver_jid:
+                    self.send_message(mto=receiver_jid, mbody=str(msg))
+            self.sent_messages.add(msg["id"])
 
-                self.send_message(mto=receiver, mbody=str(msg))
-        except:
-            print("No hay nodos")
-
-    # When user receives message
     def message(self, msg):
-
         if msg["type"] in ("chat", "normal"):
             msg_f = eval(msg["body"])
-            node = list(self.json_data["config"].keys())[
-                list(self.json_data["config"].values()).index(msg_f["source"])
-            ]
+            source_node = self.get_key_by_value(msg_f["source"])
 
-            if self.flag == False:
-                self.nodes_visited.append(node)
+            if not self.flag:
+                self.nodes_visited.append(source_node)
                 self.flag = True
             else:
-                for node in msg_f["nodes"]:
-                    if node in self.nodes_visited:
-                        self.run = True
+                self.run = any(node in self.nodes_visited for node in msg_f["nodes"])
 
-            if self.run == False:
+            self.display_message(msg_f)
 
-                if msg_f["destination"] == self.jid:
-                    print("LLEGUE A MI DESTINO")
-                    print("\nMensaje recibido de: ", msg_f["source"])
-                    print("Mensaje para: ", msg_f["destination"])
-                    print("Mensaje: ", msg_f["message"])
-                    print("Saltos: ", msg_f["hops"])
-                    print("Distancia: ", msg_f["distance"])
-                    print("Nodos: ", msg_f["nodes"])
-                    print("\n")
-                else:
-                    print("\nMensaje recibido de: ", msg_f["source"])
-                    print("Mensaje para: ", msg_f["destination"])
-                    print("Mensaje: ", msg_f["message"])
-                    print("Saltos: ", msg_f["hops"])
-                    print("Distancia: ", msg_f["distance"])
-                    print("Nodos: ", msg_f["nodes"])
-                    print("\n")
-                    print(
-                        "Getting key: ",
-                        list(self.json_data["config"].keys())[
-                            list(self.json_data["config"].values()).index(self.jid)
-                        ],
-                    )
-                    node = list(self.json_data["config"].keys())[
-                        list(self.json_data["config"].values()).index(self.jid)
-                    ]
-                    try:  # cuando ya no hay mas nodos a quien enviar desde un nodo
-                        receivers_node = self.topology["config"][node]
-                        print("send message to: ", receivers_node)
-                        msg_f["hops"] = msg_f["hops"] + 1
-                        msg_f["nodes"].append(node)
-                        msg_f["distance"] = msg_f["distance"] + 1
-                        for receiver in receivers_node:
-                            receiver = self.json_data["config"][receiver]
-                            self.send_message(mto=receiver, mbody=str(msg_f))
-                        self.nodes_visited.append(node)
-                    except:
-                        print("No hay nodos")
+            node = self.get_key_by_value(self.jid)
+            if not self.run and node:
+                self.process_node(msg_f, node)
 
-
-"""
-Class to manage the registration of a user
-ARGS:
-    jid: username to be registered
-    password: password of the user
-"""
-
-
-class RegisterChat(slixmpp.ClientXMPP):
-    def __init__(self, jid, password):
-        slixmpp.ClientXMPP.__init__(self, jid, password)
-        self.add_event_handler("session_start", self.session_start)
-        self.add_event_handler("register", self.registration_user)
-        self.register_plugin("xep_0030")  # Service Discovery
-        self.register_plugin("xep_0004")  # Data Forms
-        self.register_plugin("xep_0066")  # Out-of-band Data
-        self.register_plugin("xep_0077")  # In-band Registration
-
-    """
-    Function to handle the session start
-    ARGS:
-        event: event that is triggered when the session starts
-    """
-
-    async def session_start(self, event):
-        self.send_presence()
-        await self.get_roster()
-
-    """
-    Function to register an user in an iq request
-    ARGS:
-        iq: iq request to register a user
-    """
-
-    async def registration_user(self, iq):
-        event = self.Iq()
-        event["type"] = "set"
-        event["register"]["username"] = self.boundjid.user
-        event["register"]["password"] = self.password
-        try:
-            await event.send()
-            print("Usuario registrado con exito")
-        except slixmpp.exceptions.IqError as e:
-            logging.error("Could not register: %s", e.iq["error"]["text"])
-            self.disconnect()
-        except slixmpp.exceptions.IqTimeout:
-            logging.error("No response from server.")
-            self.disconnect()
-        finally:
-            self.disconnect()
+    def display_message(self, msg_f):
+        if msg_f["destination"] == self.jid:
+            print("\nHe llegado a mi destino")
+        print("\nMensaje recibido de:", msg_f["source"])
+        print("Mensaje para:", msg_f["destination"])
+        print("Mensaje:", msg_f["message"])
+        print("Saltos:", msg_f["hops"])
+        print("Distancia:", msg_f["distance"])
+        print("Nodos:", msg_f["nodes"])
+        print("\n")
